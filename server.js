427279f6c653215d8c41a2a64ad94406da9ab74a
Jonas Gierer <jgierer12@gmail.com>
2018-12-05 03:54:05 +0100

    Run custom server without webpack

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-// proxy for `gatsby develop` to enable CORS
-const { spawn } = require(`child_process`)
+const path = require(`path`)
 const express = require(`express`)
+const graphqlHTTP = require(`express-graphql`)
 const cors = require(`cors`)
-const proxy = require(`express-http-proxy`)
 
-const app = express()
-app.use(cors())
-let running = false
+const { store } = require(`gatsby/dist/redux`)
+const bootstrap = require(`gatsby/dist/bootstrap`)
 
-console.log(`starting \`gatsby develop\``)
-const gatsbyDevelop = spawn(`${__dirname}/node_modules/.bin/gatsby`, [
-  `develop`,
-  `--host`,
-  `0.0.0.0`,
-])
-gatsbyDevelop.stdout.on(`data`, data => {
-  if (data.toString().includes(`Compiled successfully`) && !running) {
-    app.use(`/`, proxy(`http://0.0.0.0:8000`))
+const server = async program => {
+  let { port, host } = program
+  port = typeof port === `string` ? parseInt(port, 10) : port
 
-    const port = process.env.PORT || 9000
-    app.listen(port)
-    console.log(`listening on port ${port}`)
+  // bootstrap to ensure schema is in the store
+  await bootstrap(program)
 
-    running = true
-  }
+  const schema = store.getState().schema
+
+  const app = express()
+  app.use(cors())
+  app.use(
+    `/`,
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    })
+  )
+
+  console.log(`Gatsby API server running at`, `http://${host}:${port}`)
+  app.listen(port, host)
+}
+
+server({
+  port: process.env.PORT || 8080,
+  host: `0.0.0.0`,
+  directory: __dirname,
+  sitePackageJson: require(path.join(__dirname, `package.json`)),
+  // make Gatsby think this is a normal `gatsby build` command
+  _: [`build`],
 })
